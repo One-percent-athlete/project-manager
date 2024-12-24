c3cd040ac2ef28237e95f97c73b3ec9d48f03fe9
One-percent-athlete <one.percent.athlete@gmail.com>
2024-12-24 23:58:42 +0900

    Update side nav bar

--- src/componets/Sidebar/SideNav.tsx
+++ src/componets/Sidebar/SideNav.tsx
@@ -3,7 +3,11 @@ import React from 'react'
 const SideNav = () => {
   return (
     <aside className="fixed top-0 left-0 z-40 w-64 h-screen pt-20  bg-white border-r border-gray-200">
-        <div className="h-full px-3 pb-4 overflow-y-auto bg-white"></div>    
+        <div className="h-full px-3 pb-4 overflow-y-auto bg-white">
+            <ul className="space-y-2 font-medium">
+                
+            </ul>    
+        </div>    
         SideNav
     </aside>
   )
