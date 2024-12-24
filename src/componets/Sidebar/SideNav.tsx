a52ea9039e199339c8e4cba888506ff0319767d5
One-percent-athlete <one.percent.athlete@gmail.com>
2024-12-24 23:55:33 +0900

    Update side nav bar

--- src/componets/Sidebar/SideNav.tsx
+++ src/componets/Sidebar/SideNav.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SideNav = () => {
   return (
-    <div className="fixed top-0 left-0 z-40 w-64 h-screen">SideNav</div>
+    <div className="fixed top-0 left-0 z-40 w-64 h-screen pt-20  bg-white border-r border-gray-200">SideNav</div>
   )
 }
 
