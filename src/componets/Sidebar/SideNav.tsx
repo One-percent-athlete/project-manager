42f073978e6127003a14775c5aff8fab78773cd6
One-percent-athlete <one.percent.athlete@gmail.com>
2024-12-25 22:05:37 +0900

    Updateapp page and sidenav

--- src/componets/Sidebar/SideNav.tsx
+++ src/componets/Sidebar/SideNav.tsx
@@ -14,7 +14,7 @@ const navItems = [
 
 const SideNav = () => {
   return (
-    <aside className="fixed top-0 left-0 z-40 w-64 h-screen pt-20  bg-white border-r border-gray-200">
+    <aside className="fixed top-0 left-0 z-40 w-64 h-screen pt-20 bg-white border-r border-gray-200">
         <div className="h-full px-3 pb-4 overflow-y-auto bg-white">
             <ul className="space-y-2 font-medium">
                 { navItems. map(item => <li key={item.name}>
