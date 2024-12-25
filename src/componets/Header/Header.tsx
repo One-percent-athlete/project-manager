8ec96b8e12a103ab25fcedda094daac1c68e817c
One-percent-athlete <one.percent.athlete@gmail.com>
2024-12-25 23:08:52 +0900

    Update header

--- src/componets/Header/Header.tsx
+++ src/componets/Header/Header.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <nav className="fixed top-0 left-0 z-50 w-full bg-white border-b border-gray-200">
+        <div className="px-3 py-3 lg:px-5 lg:pl-3">
+            <div className="flex items-center justify-between">
+                
+            </div>
+        </div>
+    </nav>
   )
 }
 
