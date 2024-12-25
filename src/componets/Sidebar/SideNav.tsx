c9dd453467041f092d4f2eb380b2cbe12582d949
One-percent-athlete <one.percent.athlete@gmail.com>
2024-12-25 21:27:06 +0900

    Import react icons

--- src/componets/Sidebar/SideNav.tsx
+++ src/componets/Sidebar/SideNav.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
+const navItems = [
+  
+]
+
 const SideNav = () => {
   return (
     <aside className="fixed top-0 left-0 z-40 w-64 h-screen pt-20  bg-white border-r border-gray-200">
