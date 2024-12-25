607108e113c101060de6cf6c91e617002d319a72
One-percent-athlete <one.percent.athlete@gmail.com>
2024-12-25 23:20:02 +0900

    Update header

--- src/componets/Header/Header.tsx
+++ src/componets/Header/Header.tsx
@@ -5,7 +5,9 @@ const Header = () => {
     <nav className="fixed top-0 left-0 z-50 w-full bg-white border-b border-gray-200">
         <div className="px-3 py-3 lg:px-5 lg:pl-3">
             <div className="flex items-center justify-between">
-                
+                <div className="flex items-center justify-start">
+                    <Link href="/"></Link>
+                </div>
             </div>
         </div>
     </nav>
