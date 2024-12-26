b512b77c23349f791ecc0e0b89cfb8cd0d3c0133
One-percent-athlete <one.percent.athlete@gmail.com>
2024-12-26 22:34:23 +0900

    Update header tsx

--- src/componets/Header/Header.tsx
+++ src/componets/Header/Header.tsx
@@ -21,7 +21,10 @@ const Header = () => {
                             </button>
                         </div>
                         <div className="z-50 right-0 absolute my-4 text-base list-none bg-white devide-y devide-gray-100 rounded shadow">
-                            
+                            <div className="px-4 py-3">
+                                <p className="text-sm text-gray-900">
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
