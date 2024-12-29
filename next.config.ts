62dd45d4515d83e53fb4b42bd3ca072ba0bda6b2
One-percent-athlete <one.percent.athlete@gmail.com>
2024-12-29 23:09:37 +0900

    Update next config ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  iamges: {
+    domains: ["avatars.githubusercontent.com"]
+  }
 };
 
 export default nextConfig;
