025eb047daa2963773f6226a25da141f26d8492c
One-percent-athlete <one.percent.athlete@gmail.com>
2025-01-02 23:00:06 +0900

    Create project table

---
+++ src/components/ProjectTable/ProjectTable.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+const ProjectTable = () => {
+  return (
+    <div className="relative overflow-x-auto shadow-md sm:rounded-lg mt-8">
+        <table className="w-full text-sm text-left text-gray-500">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+                <tr>
+                    <th scope="col" className="px-6 py-3">
+                        Name
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                        Description
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                        Created At
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                        <span className="sr-only">Edit</span>
+                    </th>
+                </tr>
+            </thead>
+        </table>
+    </div>
+  )
+}
+
+export default ProjectTable
