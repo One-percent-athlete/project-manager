0585151147a1e88ec6d3a15d195374947d22ab6e
One-percent-athlete <one.percent.athlete@gmail.com>
2025-01-02 23:13:49 +0900

    Update project table

--- src/components/ProjectTable/ProjectTable.tsx
+++ src/components/ProjectTable/ProjectTable.tsx
@@ -1,6 +1,17 @@
 "use client"
 
-const ProjectTable = () => {
+import { Project } from "@/models/projects"
+import { format } from "date-fns"
+import Link from "next/link"
+import { FC } from "react"
+
+type Props = {
+    projects: Project[]
+}
+
+const ProjectTable: FC<Props> = (props) => {
+    const {projects} = props
+
   return (
     <div className="relative overflow-x-auto shadow-md sm:rounded-lg mt-8">
         <table className="w-full text-sm text-left text-gray-500">
@@ -20,6 +31,23 @@ const ProjectTable = () => {
                     </th>
                 </tr>
             </thead>
+            <tbody>
+                {projects.map(project => {
+
+                    const formattedDate = format(new Date(project.createdAt))
+
+                    return (
+                        <tr key={project.id} className="bg-white border-b hover:bg-gray-50">
+                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap cursor-pointer underline">
+                                <Link href={`/project/${project.slug}`}>{project.name}</Link>
+                            </th>
+                            <td className="px-6 py-4 ">{project.description}</td>
+                            <td className="px-6 py-4 ">{project.description}</td>
+                            <td className="px-6 py-4 ">{project.description}</td>
+                        </tr>
+                    )
+                })}
+            </tbody>
         </table>
     </div>
   )
