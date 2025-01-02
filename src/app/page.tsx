27062fa364399eb6625b421f1d737a8214eb9611
One-percent-athlete <one.percent.athlete@gmail.com>
2025-01-02 22:53:11 +0900

    Create project model and update homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 "use client"
 
+import { Project } from "@/models/projects";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
 export default function Home() {
 
-  const [ projects, setProjects ] = useState<null | []>(null)
+  const [ projects, setProjects ] = useState<null | Project>(null)
 
   useEffect(() => {
     const fetchProjects = async () => {
