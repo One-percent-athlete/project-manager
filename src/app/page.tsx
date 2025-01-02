901b10d5dd818936c630cb0b93c533f141fc9fc9
One-percent-athlete <one.percent.athlete@gmail.com>
2025-01-02 22:49:33 +0900

    Update app homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
-import Image from "next/image";
-import { useEffect } from "react";
+"use client"
+
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  useEffect(() => {}, [])
+
+  const [ projects, setProjects ] = useState<null | []>(null)
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      const { data } = await axios.get("/api/projects")
+      console.log(data);
+      setProjects(data)
+    }
+
+    fetchProjects()
+  }, [])
   return (
     <>Home</>
   );
