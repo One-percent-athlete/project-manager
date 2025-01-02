b24a498153c90572538ac5199ff2306fa0f88870
One-percent-athlete <one.percent.athlete@gmail.com>
2025-01-02 22:19:56 +0900

    Create project route

---
+++ src/app/api/projects/route.ts
@@ -0,0 +1,24 @@
+import { authOptions } from "@/libs/auth";
+import prisma from "@/libs/prisma";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request, res: Response) {
+    const session = await getServerSession(authOptions)
+
+    if(!session){
+        return new NextResponse("Not Authenticated", {status: 500})
+    }
+
+    try {
+        const products = await prisma.project.findMany()
+
+        return NextResponse.json(products, {
+            status: 200,
+            statusText: "Successful"
+        })
+    } catch (error) {
+        console.log(error);
+        return new NextResponse("Cannot fetch data", {status: 500})
+    }
+}
