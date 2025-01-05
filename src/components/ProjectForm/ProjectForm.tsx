b3717c53b3fbaefb1a4d97b471d7a588347c4a9a
One-percent-athlete <one.percent.athlete@gmail.com>
2025-01-05 21:32:33 +0900

    Update project form component

--- src/components/ProjectForm/ProjectForm.tsx
+++ src/components/ProjectForm/ProjectForm.tsx
@@ -8,10 +8,11 @@ interface ProjectFormProps {
     handleSubmit: (event: FormEvent<HTMLFormElement>) => void
     onChange: (event: ChangeEvent<HTMLInputElement>) => void
     formData: {name: string, description: string}
+    isCreateProject: boolean
 }
 
 const ProjectForm: FC<ProjectFormProps> = props => {
-    const { isVisible, toggleProjectForm, handleSubmit, onChange, formData } = props
+    const { isVisible, toggleProjectForm, handleSubmit, onChange, formData, isCreateProject } = props
 
     return <div className={`absolute w-80 z-[55] bg-white rounded-lg shadow ${isVisible ? "left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transition-transform duration-700" : "-translate-y-[150%] left-1/2 -translate-x-1/2"}`}>
         <button type='button' className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200  hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center">
@@ -23,6 +24,10 @@ const ProjectForm: FC<ProjectFormProps> = props => {
             <form className="space-y-6" onSubmit={handleSubmit}>
                 <Input label="Name" onChange={onChange} placeholder="Name" type="text" value={formData.name} name="name" required />
                 <Input label="Description" onChange={onChange} placeholder="Description" type="text" value={formData.description} name="description" required />
+
+                <button disabled={isCreateProject} type="submit" className="disabled:bg-gray-300 w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 pyr-2.5 text-center">
+                    {isCreateProject ? "Creating..." : "Create"}
+                </button>
             </form>
         </div>
     </div>
