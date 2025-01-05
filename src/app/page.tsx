b4ad2f7822cbe635a909bd21d8dbb21b110d1c1e
One-percent-athlete <one.percent.athlete@gmail.com>
2025-01-05 21:47:54 +0900

    Update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import ProjectForm from "@/components/ProjectForm/ProjectForm";
 import ProjectTable from "@/components/ProjectTable/ProjectTable";
 import { Project } from "@/models/projects";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 
 export default function Home() {
 
@@ -17,6 +17,7 @@ export default function Home() {
 
   const [ projects, setProjects ] = useState<null | Project[]>(null)
   const [ showForm, setShowForm ] = useState(false)
+  const [ isCreateProject, setIsCreateProject ] = useState(false)
   const [ formData, setFormData ] = useState<FormData>({name: "", description: "", id: ""})
 
   useEffect(() => {
@@ -28,10 +29,28 @@ export default function Home() {
     fetchProjects()
   }, [])
   
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    setIsCreateProject(true)
+
+    const slug = slugify(formData.name)
+
+    try {
+      const { statusText } = await axios.post("/api/projects", {
+        name: formData.name,
+        description: formData.description,
+        slug
+      })
+    } catch (error) {
+      
+    }
+  }
+
   return (
     <>
       <Modal isVisible={showForm} />
-      <ProjectForm formData={formData} isVisible={showForm} toggleProjectForm={() => {}} handleSubmit={} />
+      <ProjectForm formData={formData} isVisible={showForm} toggleProjectForm={() => {}} handleSubmit={handleSubmit} isCreateProject={isCreateProject} />
       <button className="relative outline-none inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden">
         <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white rounded-md group-hover:first-line:ring-opacity-45">
           Create Project
@@ -41,3 +60,7 @@ export default function Home() {
     </>
   );
 }
+function slugify(name: string) {
+  throw new Error("Function not implemented.");
+}
+
