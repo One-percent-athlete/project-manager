71d8051018c817595f8bf9889a7fa7eacc4db3a6
One-percent-athlete <one.percent.athlete@gmail.com>
2025-01-05 21:40:10 +0900

    Update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,15 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
 
+  interface FormData {
+    name: string, 
+    description: string, 
+    id?:string
+  }
+
   const [ projects, setProjects ] = useState<null | Project[]>(null)
+  const [ showForm, setShowForm ] = useState(false)
+  const [ formData, setFormData ] = useState<FormData>({name: "", description: "", id: ""})
 
   useEffect(() => {
     const fetchProjects = async () => {
@@ -19,10 +27,11 @@ export default function Home() {
 
     fetchProjects()
   }, [])
+  
   return (
     <>
-      <Modal isVisible />
-      <ProjectForm isVisible toggleProjectForm={() => {}} />
+      <Modal isVisible={showForm} />
+      <ProjectForm formData={formData} isVisible={showForm} toggleProjectForm={() => {}} handleSubmit={} />
       <button className="relative outline-none inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden">
         <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white rounded-md group-hover:first-line:ring-opacity-45">
           Create Project
