9c9edddd5c42e034be4f21d1da901bad9919e33f
One-percent-athlete <one.percent.athlete@gmail.com>
2025-01-05 21:25:45 +0900

    Update projecgt form component

--- src/components/ProjectForm/ProjectForm.tsx
+++ src/components/ProjectForm/ProjectForm.tsx
@@ -1,15 +1,17 @@
 import { ChangeEvent, FC, FormEvent } from "react"
 import { MdCancel } from "react-icons/md"
+import Input from "../Inpit/Input"
 
 interface ProjectFormProps {
     isVisible: boolean
     toggleProjectForm: () => void
     handleSubmit: (event: FormEvent<HTMLFormElement>) => void
     onChange: (event: ChangeEvent<HTMLInputElement>) => void
+    formData: {name: string, description: string}
 }
 
 const ProjectForm: FC<ProjectFormProps> = props => {
-    const { isVisible, toggleProjectForm, handleSubmit, onChange } = props
+    const { isVisible, toggleProjectForm, handleSubmit, onChange, formData } = props
 
     return <div className={`absolute w-80 z-[55] bg-white rounded-lg shadow ${isVisible ? "left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transition-transform duration-700" : "-translate-y-[150%] left-1/2 -translate-x-1/2"}`}>
         <button type='button' className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200  hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center">
@@ -19,8 +21,8 @@ const ProjectForm: FC<ProjectFormProps> = props => {
         <div className="px-6 py-6 lg:px-8">
             <h3 className="mb-4 text-xl font-medium text-gray-900">Add Project</h3>
             <form className="space-y-6" onSubmit={handleSubmit}>
-                <Input label="Name" onChange={onChange} placeholder="Name" type="text" value={} name="name" required />
-                <Input label="Description" onChange={onChange} placeholder="Description" type="text" value={} name="description" required />
+                <Input label="Name" onChange={onChange} placeholder="Name" type="text" value={formData.name} name="name" required />
+                <Input label="Description" onChange={onChange} placeholder="Description" type="text" value={formData.description} name="description" required />
             </form>
         </div>
     </div>
