dd4b0a5ac8df9e5db5430fd4cf410f5e17f1bbab
One-percent-athlete <one.percent.athlete@gmail.com>
2025-01-05 21:12:21 +0900

    Update project form component

--- src/components/ProjectForm/ProjectForm.tsx
+++ src/components/ProjectForm/ProjectForm.tsx
@@ -3,14 +3,15 @@ import { MdCancel } from "react-icons/md"
 
 interface ProjectFormProps {
     isVisible: boolean
+    toggleProjectForm: () => void
 }
 
 const ProjectForm: FC<ProjectFormProps> = props => {
-    const { isVisible } = props
+    const { isVisible, toggleProjectForm } = props
 
     return <div className={`absolute w-80 z-[55] bg-white rounded-lg shadow ${isVisible ? "left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transition-transform duration-700" : "-translate-y-[150%] left-1/2 -translate-x-1/2"}`}>
         <button type='button' className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200  hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center">
-            <MdCancel className="text-3xl" />
+            <MdCancel onClick={toggleProjectForm} className="text-3xl" />
         </button>
     </div>
 }
