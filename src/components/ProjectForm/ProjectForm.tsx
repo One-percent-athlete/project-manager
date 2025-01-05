e15f06ac2b001e4db23b3666e9083cdf076c68bf
One-percent-athlete <one.percent.athlete@gmail.com>
2025-01-05 21:13:59 +0900

    Update project form component

--- src/components/ProjectForm/ProjectForm.tsx
+++ src/components/ProjectForm/ProjectForm.tsx
@@ -13,6 +13,10 @@ const ProjectForm: FC<ProjectFormProps> = props => {
         <button type='button' className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200  hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center">
             <MdCancel onClick={toggleProjectForm} className="text-3xl" />
         </button>
+
+        <div className="px-6 py-6 lg:px-8">
+            <h3 className="mb-4 text-xl font-medium text-gray-900">Add Project</h3>
+        </div>
     </div>
 }
 
