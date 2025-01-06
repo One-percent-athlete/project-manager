b3bf5eb2eccda8df2d0afa22f2783e131467c0f0
One-percent-athlete <one.percent.athlete@gmail.com>
2025-01-06 22:01:33 +0900

    Update homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import ProjectTable from "@/components/ProjectTable/ProjectTable";
 import { Project } from "@/models/projects";
 import axios from "axios";
 import { ChangeEvent, FormEvent, useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 export default function Home() {
 
@@ -27,30 +28,47 @@ export default function Home() {
     }
 
     fetchProjects()
-  }, [])
+  }, []);
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target
+
+    setFormData(prevData => ({
+      ...prevData,
+      [name]: value,
+    }));
+  }
   
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault()
 
     setIsCreateProject(true)
 
-    const slug = slugify(formData.name)
+    // const slug = slugify(formData.name)
 
-    try {
-      const { statusText } = await axios.post("/api/projects", {
-        name: formData.name,
-        description: formData.description,
-        slug
-      })
-    } catch (error) {
-      
-    }
+    // try {
+    //   const { statusText } = await axios.post("/api/projects", {
+    //     name: formData.name,
+    //     description: formData.description,
+    //     slug
+    //   })
+
+    //   toast.success(statusText)
+    // } catch (error: any) {
+    //   toast.error(error.response.data)
+    // } finally {
+    //   setFormData({ name: "", description: ""})
+    //   setShowForm(false)
+    //   setIsCreateProject(false)
+    // }
   }
+
+  const toggleProjectForm = () => setShowForm(prevState => !prevState)
 
   return (
     <>
       <Modal isVisible={showForm} />
-      <ProjectForm formData={formData} isVisible={showForm} toggleProjectForm={() => { } } handleSubmit={handleSubmit} isCreateProject={isCreateProject} onChange={() => {} } />
+      <ProjectForm formData={formData} isVisible={showForm} toggleProjectForm={toggleProjectForm} handleSubmit={handleSubmit} isCreateProject={isCreateProject} onChange={handleInputChange} />
       <button className="relative outline-none inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden">
         <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white rounded-md group-hover:first-line:ring-opacity-45">
           Create Project
