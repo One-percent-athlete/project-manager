b586e42045f03217c5227656a50f94350ed742a3
One-percent-athlete <one.percent.athlete@gmail.com>
2025-01-07 18:43:07 +0900

    Create project board route

---
+++ src/app/api/project-board/route.ts
@@ -0,0 +1,36 @@
+import prisma from "@/libs/prisma"
+import { connect } from "http2"
+import { NextResponse } from "next/server"
+
+export async function POST(req: Request, res:Response) {
+    const {status, projectId, slug} = await req.json()
+
+    if (!status || !projectId || !slug) 
+        return new NextResponse("All fields are required", {status: 400})
+
+    try {
+        const maxOrderResult = await prisma.projectBoard.aggregate({
+            _max: {
+                order: true
+            },
+            where: {
+                projectId
+            }
+        })
+
+        const nextOrder = maxOrderResult._max?.order ? maxOrderResult._max.order + 1 : 1
+
+        const createProjectBoard = await prisma.projectBoard.create({
+            data: {
+                slug,
+                status,
+                project: {
+                    connect: {id: projectId}
+                },
+                order: nextOrder
+            }
+        })
+    } catch (error) {
+        return new NextResponse("Creation Error", {status: 500})
+    }
+}
