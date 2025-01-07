6a70744ec680fd7839fc687e148e5a2477c4b33a
One-percent-athlete <one.percent.athlete@gmail.com>
2025-01-07 18:28:47 +0900

    Update project api route

--- src/app/api/projects/[slug]/route.tsx
+++ src/app/api/projects/[slug]/route.tsx
@@ -1,10 +1,18 @@
 import prisma from "@/libs/prisma";
 import { NextResponse } from "next/server";
+import { features } from "process";
 
 export async function GET(req: Request, { params}: any) {
     try {
         const project = await prisma.project.findUnique({
-            where: {slug: params.slug}
+            where: {slug: params.slug},
+            include: {
+                projectBoards: {
+                    include: {
+                        features: true
+                    }
+                }
+            }
         })
 
         return NextResponse.json(project, {status: 200, statusText: "Successful"})
