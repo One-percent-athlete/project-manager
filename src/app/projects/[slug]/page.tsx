9deecff1d629a38e08a1c48862db1cc8d38c6a98
One-percent-athlete <one.percent.athlete@gmail.com>
2025-01-07 18:59:18 +0900

    Update project item page

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import AddBoardForm from "@/components/AddBoardForm/AddBoardForm"
+import Modal from "@/components/Modal/Modal"
 import { Project } from "@/models/projects"
 import axios from "axios"
 import { useParams } from "next/navigation"
@@ -19,19 +20,23 @@ const ProjectItem = () => {
         fetchProject()
     }, [slug])
 
-    if (!project) return<></>
-  return 
-  <>
-    <AddBoardForm isVisible />
-    <div className="mb-6">
-        <h4 className="text-2xl font-bold">{project?.name}</h4> 
-        <p className="text-base text-gray-600">{project?.description}</p> 
-    </div>
+    if (!project) {
+        return <></>
+    } else {
+        return 
+        <>
+            <Modal isVisible />
+            <AddBoardForm isVisible />
+            <div className="mb-6">
+                <h4 className="text-2xl font-bold">{project?.name}</h4> 
+                <p className="text-base text-gray-600">{project?.description}</p> 
+            </div>
 
-    <div className="grid place-content-center hover:bg-[#f5f5f5] cursor-pointer rounded-2xl border-4 border-dotted flex-none w-[354px] h-20 py-7">
-        <AiFillPlusCircle className="text-6xl" />
-    </div>
-  </>
+            <div className="grid place-content-center hover:bg-[#f5f5f5] cursor-pointer rounded-2xl border-4 border-dotted flex-none w-[354px] h-20 py-7">
+                <AiFillPlusCircle className="text-6xl" />
+            </div>
+        </>
+    }
 }
 
 export default ProjectItem
