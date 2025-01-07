92c65d637c29da7c9b529c4bb583f80dedbbb100
One-percent-athlete <one.percent.athlete@gmail.com>
2025-01-07 18:25:24 +0900

    Update project item page

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,11 +1,12 @@
 "use client"
 
+import { Project } from "@/models/projects"
 import axios from "axios"
 import { useParams } from "next/navigation"
 import React, { useEffect, useState } from 'react'
 
 const ProjectItem = () => {
-    const [project, setProject] = useState<null>(null)
+    const [project, setProject] = useState<null | Project>(null)
     const { slug } = useParams()
 
     useEffect(() => {
@@ -15,9 +16,15 @@ const ProjectItem = () => {
 
         fetchProject()
     }, [slug])
-  return (
-    <div>ProjectItem</div>
-  )
+
+    if (!project) return<></>
+  return 
+  <>
+    <div className="mb-6">
+        <h4 className="text-2xl font-bold">{project?.name}</h4> 
+        <p className="text-base text-gray-600">{project?.description}</p> 
+    </div>
+  </>
 }
 
 export default ProjectItem
