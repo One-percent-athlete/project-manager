b87c5e82f60ad238da9e9d5f8873ad48b29f39da
One-percent-athlete <one.percent.athlete@gmail.com>
2025-01-07 19:01:24 +0900

    Update add board form

--- src/components/AddBoardForm/AddBoardForm.tsx
+++ src/components/AddBoardForm/AddBoardForm.tsx
@@ -3,14 +3,15 @@ import { MdCancel } from "react-icons/md"
 
 type Props = {
     isVisible: boolean
+    toggleAddBoardForm: () => void
 }
 
 const AddBoardForm: FC<Props> = props => {
-    const { isVisible } = props
+    const { isVisible, toggleAddBoardForm } = props
 
   return <div className={`absolute w-80 z-[55] bg-gray-200 rounded-lg shadow ${isVisible ? "left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transition-transform duration-700" : "-translate-y-[150%] left-1/2 -translate-x-1/2"}`}>
     <button type="button" className="absolute top-3 right-2.5 text-gray-400 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center bg-gray-600">
-        <MdCancel className="text-3xl" />
+        <MdCancel onClick={toggleAddBoardForm} className="text-3xl" />
     </button>
     </div>
 }
