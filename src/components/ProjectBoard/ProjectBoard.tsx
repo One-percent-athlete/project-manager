17ec40909e44107bdc030f2e88f9367eb771bb9d
One-percent-athlete <one.percent.athlete@gmail.com>
2025-01-08 21:48:55 +0900

    Update project board

--- src/components/ProjectBoard/ProjectBoard.tsx
+++ src/components/ProjectBoard/ProjectBoard.tsx
@@ -20,6 +20,11 @@ const ProjectBoard: FC<Props> = props => {
             <p className="fojnt-medium mr-3">{boardHeading}</p>
             <p className="w-6 h-6 text-center flex items-center justify-center bg-[#e0e0e0] text-[#625f6] rounded-full">{numFeatures}</p>
         </div>
+
+        <button onClick={() => {
+            toggleAddFeature()
+            setSelectBoardId(boardId)
+        }} className="text-white bg-[#30e575] px-3 py-1 rounded-lg font-medium text-lg">+</button>
     </div>)
 }
 
