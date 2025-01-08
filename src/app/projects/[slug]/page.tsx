5b60a26918ac8815cbe85bc2ff6639879a879bef
One-percent-athlete <one.percent.athlete@gmail.com>
2025-01-08 20:42:34 +0900

    Update project page

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -10,6 +10,10 @@ import { AiFillPlusCircle } from "react-icons/ai"
 
 const ProjectItem = () => {
     const [project, setProject] = useState<null | Project>(null)
+    const [isAddBoardFormVisible, setIsAddBoardFormVisible ] = useState(false)
+    const [isSubmitting, setIsSubmitting ] = useState(false)
+    const [boardData, setBoardData] = useState({status: ""})
+
     const { slug } = useParams()
 
     useEffect(() => {
@@ -20,23 +24,21 @@ const ProjectItem = () => {
         fetchProject()
     }, [slug])
 
-    if (!project) {
-        return <></>
-    } else {
-        return 
-        <>
-            <Modal isVisible />
-            <AddBoardForm isVisible />
-            <div className="mb-6">
-                <h4 className="text-2xl font-bold">{project?.name}</h4> 
-                <p className="text-base text-gray-600">{project?.description}</p> 
-            </div>
+    if (!project) return <></>
+    const toggleAddBoardForm = () => setIsAddBoardFormVisible(prevState => !prevState)
 
-            <div className="grid place-content-center hover:bg-[#f5f5f5] cursor-pointer rounded-2xl border-4 border-dotted flex-none w-[354px] h-20 py-7">
-                <AiFillPlusCircle className="text-6xl" />
-            </div>
-        </>
-    }
+    return (
+    <>
+        <Modal isVisible={isAddBoardFormVisible} />
+        <AddBoardForm isVisible={isAddBoardFormVisible} toggleAddBoardForm={toggleAddBoardForm} boardData={boardData} handleBoardSubmit={} isSubmitting={isSubmitting} updateBoardHandler={} />
+        <div className="mb-6">
+            <h4 className="text-2xl font-bold">{project?.name}</h4> 
+            <p className="text-base text-gray-600">{project?.description}</p> 
+        </div>
+
+        <div className="grid place-content-center hover:bg-[#f5f5f5] cursor-pointer rounded-2xl border-4 border-dotted flex-none w-[354px] h-20 py-7">
+            <AiFillPlusCircle className="text-6xl" />
+        </div>
+    </>)
 }
-
 export default ProjectItem
