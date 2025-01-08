7e29bf174c8e0365ad524dea109b3750451cd7fc
One-percent-athlete <one.percent.athlete@gmail.com>
2025-01-08 21:33:40 +0900

    Update project board

--- src/components/ProjectBoard/ProjectBoard.tsx
+++ src/components/ProjectBoard/ProjectBoard.tsx
@@ -1,6 +1,16 @@
 "use client"
 
-const ProjectBoard = () => {
+import { Dispatch, FC, SetStateAction } from "react"
+
+type Props = {
+    boardHeading: string
+    toggleAddFeature: () => void
+    setSelectBoardId: Dispatch<SetStateAction<string>>
+    boardId: string
+    numFeature: number
+}
+
+const ProjectBoard: FC<Props> = props => {
   return (
     <div>ProjectBoard</div>
   )
