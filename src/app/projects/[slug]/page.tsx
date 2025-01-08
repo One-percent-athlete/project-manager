fdad3a09c18068d24c504a473a97cf660b048bd0
One-percent-athlete <one.percent.athlete@gmail.com>
2025-01-08 21:26:35 +0900

    Update project page

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -65,9 +65,12 @@ const ProjectItem = () => {
             <p className="text-base text-gray-600">{project?.description}</p> 
         </div>
 
-        <div onClick={toggleAddBoardForm} className="grid place-content-center hover:bg-[#f5f5f5] cursor-pointer rounded-2xl border-4 border-dotted flex-none w-[354px] h-20 py-7">
-            <AiFillPlusCircle className="text-6xl" />
+        <div className="flex gap-6 items-start">
+            <div onClick={toggleAddBoardForm} className="grid place-content-center hover:bg-[#f5f5f5] cursor-pointer rounded-2xl border-4 border-dotted flex-none w-[354px] h-20 py-7">
+                <AiFillPlusCircle className="text-6xl" />
+            </div>
         </div>
+
     </>)
 }
 export default ProjectItem
