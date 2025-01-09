ed8599f6f109dec63a5ca1782e16f8b97715d2c5
One-percent-athlete <one.percent.athlete@gmail.com>
2025-01-09 23:53:53 +0900

    Update add featue

--- src/components/AddFeatureForm/AddFeatureForm.tsx
+++ src/components/AddFeatureForm/AddFeatureForm.tsx
@@ -5,7 +5,11 @@ type Props = {
     toggleAddFeatureForm: () => void
     handleFeatureSubmit: (event:FormEvent<HTMLFormElement>) => Promise<void>
     handleFeatureChange: (event:ChangeEvent<HTMLInputElement>) => void
-        
+    featureFormData: {
+        name: string
+        description: string
+        finishData: string
+    }
 }
 
 const AddFeatureForm: FC<Props> = (props) => {
