e3029a52b387b2881122374ae2537e75938a0b2f
One-percent-athlete <one.percent.athlete@gmail.com>
2025-01-10 22:54:10 +0900

    Update add feature form

--- src/components/AddFeatureForm/AddFeatureForm.tsx
+++ src/components/AddFeatureForm/AddFeatureForm.tsx
@@ -1,4 +1,5 @@
 import React, { ChangeEvent, FC, FormEvent } from 'react'
+import { MdCancel } from "react-icons/md"
 
 type Props = {
     isVisible: boolean
@@ -16,7 +17,11 @@ const AddFeatureForm: FC<Props> = (props) => {
     const { isVisible, featureFormData, handleFeatureChange, handleFeatureSubmit, toggleAddFeatureForm } = props
 
   return (
-    <div>AddFeatureForm</div>
+    <div className={`absolute w-80 z-[55] bg-gray-200 rounded-lg shadow ${isVisible ? "left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transition-transform duration-700" : "-translate-y-[150%] left-1/2 -translate-x-1/2"}`}>
+        <button type="button" className="absolute top-3 right-2.5 text-gray-400 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center bg-gray-600">
+            <MdCancel onClick={toggleAddFeatureForm} className="text-3xl" />
+        </button>
+    </div>
   )
 }
 
