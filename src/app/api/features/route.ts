ec4cb3940e42c52025bfeaeae34bdac52ea74ea6
One-percent-athlete <one.percent.athlete@gmail.com>
2025-01-10 23:15:43 +0900

    Update features api route

--- src/app/api/features/route.ts
+++ src/app/api/features/route.ts
@@ -2,7 +2,10 @@ import { NextResponse } from "next/server";
 
 export async function POST(req:Request, res:Response) {
     try {
-        
+        const {name, description, finishDate, projectBoardId, slug} = await req.json()
+
+        if (!name || !description || !finishDate || !projectBoardId || !slug) return new NextResponse("Please Provide All Infomation", {status: 400})
+
     } catch (error) {
         console.log(error);
         return new NextResponse("Creation Error", {status: 500})
