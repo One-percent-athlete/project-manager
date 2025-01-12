df9873e68f094b1382089a6c767e6a11c795d43c
One-percent-athlete <one.percent.athlete@gmail.com>
2025-01-12 23:18:21 +0900

    Update feature card component

--- src/components/FeatureCard/FeatureCard.tsx
+++ src/components/FeatureCard/FeatureCard.tsx
@@ -9,7 +9,11 @@ const FeatureCard: FC<Props> = (props) => {
     const { feature: {name, description, priority, finishDate} } = props
 
   return (
-    <div>FeatureCard</div>
+    <div className="my-5 bg-white p-5 rounded-2xl">
+        <span className="bg-[#dfa87433] text-[#D58D49] rounded-[4px] px-[6px] py-1 font-medium text-xs">
+            {priority}
+        </span>
+    </div>
   )
 }
 
