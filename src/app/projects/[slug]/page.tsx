2fce6b3f0caf11dc66ea43add68773a7c255d0d6
One-percent-athlete <one.percent.athlete@gmail.com>
2025-01-15 23:46:55 +0900

    Update page

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -116,22 +116,23 @@ const ProjectItem = () => {
                 {provided => (
                     <div className="flex gap-6 items-start">
                         {project.projectBoards.map(projectBoard => (
-                            <Draggable {provided => <div key={projectBoard.id} className="bg-[#f5f5f5] flex-shrink-0 w-[354px] rounded-2xl py-3 px-6">
-                            <ProjectBoard boardHeading={projectBoard.status} boardId={projectBoard.id} numFeatures={projectBoard.feature.length} setSelectBoardId={setSelectBoardId} toggleAddFeature={toggleAddFeatureForm} />
+                            <Draggable {provided => 
+                            <div key={projectBoard.id} className="bg-[#f5f5f5] flex-shrink-0 w-[354px] rounded-2xl py-3 px-6">
+                                <ProjectBoard boardHeading={projectBoard.status} boardId={projectBoard.id} numFeatures={projectBoard.feature.length} setSelectBoardId={setSelectBoardId} toggleAddFeature={toggleAddFeatureForm} />
 
-                            <div>
-                                {projectBoard.feature.map(feature => (
-                                    <FeatureCard key={feature.id} feature={feature} />
-                                ))}
+                                <div>
+                                    {projectBoard.feature.map(feature => (
+                                        <FeatureCard key={feature.id} feature={feature} />
+                                    ))}
+                                </div>
                             </div>
-                        </div>}
-                            
+                            }
                         ))}
                     </div>
-                )}
+                )
                 <div onClick={toggleAddBoardForm} className="grid place-content-center hover:bg-[#f5f5f5] cursor-pointer rounded-2xl border-4 border-dotted flex-none w-[354px] h-20 py-7">
                     <AiFillPlusCircle className="text-6xl" />
-                </div>
+                </div>}
             </Draggable>
             
 
