c6b352246db5ecf9de2159c5194a3d045ba98fdf
One-percent-athlete <one.percent.athlete@gmail.com>
2025-01-19 21:48:13 +0900

    Update route

--- src/app/api/ai/route.ts
+++ src/app/api/ai/route.ts
@@ -1,4 +1,5 @@
 import { authOptions } from "@/libs/auth"
+import prisma from "@/libs/prisma"
 import { getServerSession } from "next-auth"
 import { NextResponse } from "next/server"
 import OpenAi from "openai"
@@ -11,5 +12,24 @@ export async function POST(req: Request, res: Response) {
     const {prompt, role} = await req.json()
 
     if (!prompt || !role) return new NextResponse("Please provide a prompt", {status: 400})
-    const chatCompletion = await 
+    const chatCompletion = await openai.chat.completion.create({
+            message: [{ role, content: prompt
+            }], 
+            model: "gpt-3.5-turbo"
+        })
+    const chatMessage = [
+        {content: prompt, role},
+        chatCompletion.choices[0].message
+    ]
+    await Promise.all(chatMessage.map( async message => {
+        const { role, content } = message
+
+        await prisma.aiChat.create({
+            data: {
+                role,
+                content,
+                userId: session?.user?.id as string
+            }
+        })
+    }))
 }
