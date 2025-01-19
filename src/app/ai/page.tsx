8fcd2737ec7a05883e3e72772b73c63050892e24
One-percent-athlete <one.percent.athlete@gmail.com>
2025-01-19 22:02:39 +0900

    Update ai page

--- src/app/ai/page.tsx
+++ src/app/ai/page.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import axios from "axios"
+import React, { useEffect, useState } from 'react'
 
 const Ai = () => {
-  return (
+    const [chat, setChat] = useState<{
+        role: "user" | "assistant"
+        content: string
+        id: string
+        userId: string
+    }[]>([])
+
+    useEffect(() => {
+        const fetchUserMessages = async () => {
+            const { data } = await axios.get("/api/ai")
+        }
+    }, [])
+
+    return (
     <div>Ai</div>
   )
 }
