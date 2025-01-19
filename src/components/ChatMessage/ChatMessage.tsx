b86b4535b5debf32d0f36073de9649293ecdf1b2
One-percent-athlete <one.percent.athlete@gmail.com>
2025-01-19 22:21:07 +0900

    Update chat message

--- src/components/ChatMessage/ChatMessage.tsx
+++ src/components/ChatMessage/ChatMessage.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { FC } from 'react'
 
-const ChatMessage = () => {
-  return (
-    <div>ChatMessage</div>
-  )
+type Props {
+    text: string
+    role: "user" | "assistant"
+    userImage: string
+}
+
+const ChatMessage: FC<Props> = (props) => {
+    const { role, text, userImage } = props
+
+  return <>
+     {role === "user" && <div className="bg-white p-4 rounded-lg flex gap-4 items-center whitespace-pre-wrap">
+
+    </div> }
+  </>
 }
 
 export default ChatMessage
