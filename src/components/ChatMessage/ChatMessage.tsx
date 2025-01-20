80770e687bd8bc025f2f82741609f9272dc1839f
One-percent-athlete <one.percent.athlete@gmail.com>
2025-01-20 19:51:50 +0900

    Update chat message component

--- src/components/ChatMessage/ChatMessage.tsx
+++ src/components/ChatMessage/ChatMessage.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react'
+import { FaRobot } from "react-icons/fa"
 
 type Props {
     text: string
@@ -10,9 +11,18 @@ const ChatMessage: FC<Props> = (props) => {
     const { role, text, userImage } = props
 
   return <>
-    {role === "user" && <div className="bg-white p-4 rounded-lg flex gap-4 items-center whitespace-pre-wrap">
-
-    </div>}
+    {role === "user" && (
+        <div className="bg-white p-4 rounded-lg flex gap-4 items-center whitespace-pre-wrap">
+            <img src={userImage} alt="user" className="w-8 h-8" />
+            <p className="text-gray-700">{text}</p>
+        </div>
+    )}
+    {role === "assistant" && (
+        <div className="bg-gray-100 mb-6 p-4 rounded-lg flex gap-4 items-center whitespace-pre-wrap">
+            <FaRobot className="text-[10rem]" />
+            <p className="text-gray-700">{text}</p>
+        </div>
+    )}
   </>
 }
 
